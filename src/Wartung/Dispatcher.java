909ead4a3ea42251fe78f0ec279027ea44398f41
GollyTicker <golly.ticker@gmail.com>
2014-12-06 15:20:03 +0100

    doing Dispatcher and monitor

--- src/Wartung/Dispatcher.java
+++ src/Wartung/Dispatcher.java
@@ -1,11 +1,36 @@
 package Wartung;
 
-import Fassade.UserInterface;
+import Angebotskomponente.AngebotsNr;
+import Auftragskomponente.AuftragsNr;
+import Auftragskomponente.IAuftragServicesFuerCallCenterUI;
+import MPS.MPSConnector;
+import Utilities.NotFoundException;
+import Utilities.TechnicalException;
+
+import java.io.IOException;
 
 /**
  * Created by Swaneet on 03.12.2014.
  */
-public class Dispatcher {
-    // * Dispatcher, welcher jeweils an dem ersten freien Systmem die Anfragen abschickt.
+public class Dispatcher implements IAuftragServicesFuerCallCenterUI {
+    Monitor monitor;
+    public Dispatcher() throws IOException {
+        this.monitor = new Monitor();
+        monitor.start();
+        // TODO: DashboardGUI starten
+    }
 
+    private MPSConnector getAvailibleMPS() throws TechnicalException{
+        System.out.println("Dispaatcher: Searching for availible MPS...");
+        int port = monitor.getFirstMPSport();
+        System.out.println("Dispatcher: MPS ["+port+"]");
+        return new MPSConnector(port);
+    }
+
+    // * Dispatcher, welcher jeweils an dem ersten freien Systmem die Anfragen abschickt.
+    @Override
+    public AuftragsNr erstelleAuftrag(AngebotsNr nr) throws NotFoundException, TechnicalException {
+        MPSConnector mps = getAvailibleMPS();
+        return mps.erstelleAuftrag(nr);
+    }
 }
--- src/Wartung/Monitor.java
+++ src/Wartung/Monitor.java
@@ -1,12 +1,71 @@
 package Wartung;
 
+import MPS.MPS;
+import MPS.MPSclientAcceptor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import static MPS.MPS.MPS_BASE_PORT;
+
 /**
  * Created by Swaneet on 03.12.2014.
  */
-public class Monitor {
+public class Monitor extends Thread{
     // * Der Monitor hält sich mit dem Status der Prozesse im Laufenenden, benachrichtigt Dispatcher beim Ausfall
     //    * evtl. sind Monitor  und Dispatcher ein einziger Thread.
     public static int MONITOR_LISTENER_PORT = 9600;
     public static int ALIVE_INTERVAL_MILLIS = 100;
-    public static int ALIVE_TIMEOUT_TIMEOUT= 500;
+    public static int ALIVE_TIMEOUT_MILLIS= 500;
+    public static String ALIVE = "alive";
+    ServerSocket serverSocket;
+    public static Map<Integer, String> mpssystems = new HashMap(); // stores the responses of the reporters
+
+    public Monitor() {
+        try {
+            serverSocket = new ServerSocket(MONITOR_LISTENER_PORT);
+            System.out.println("Monitor Listener [" + MONITOR_LISTENER_PORT+"]");
+        } catch (IOException se) {
+            System.err.println("Monitor Listener: Can not start listening on port " + MONITOR_LISTENER_PORT);
+            se.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+    synchronized public void recordState(Integer mpsNum, String state) {
+        mpssystems.putIfAbsent(mpsNum, state);
+    }
+
+    synchronized public void deleteMPS(Integer mpsNum) {
+        mpssystems.remove(mpsNum);
+    }
+
+    public int getFirstMPSport() {
+        while(noAvailibleSystems()) {
+            Thread.yield();
+        }
+        for(Map.Entry<Integer, String> e:mpssystems.entrySet()) {
+            return MPS_BASE_PORT  + e.getKey();
+        }
+        throw new RuntimeException("blubb");
+    }
+
+    synchronized private boolean noAvailibleSystems() {
+        return mpssystems.isEmpty();
+    }
+
+    public void run() {
+        while (true) {  // forver keep accepting new clients.
+            try {
+                Socket socket = serverSocket.accept();
+                new ReporterHandler(socket, this).start();
+                System.out.println("Monitor Listener: Got New MPS.");
+            } catch (IOException e) {
+                System.out.println("Monitor Listener: Exception on MPS");
+                e.printStackTrace();
+            }
+        }
+    }
 }
